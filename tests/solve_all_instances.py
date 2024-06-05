9f6581673dd52a928fafc23ba584333c811c76f2
IMAI Yoshiya <imai448@gmail.com>
2024-06-05 20:08:19 +0900

    テスト（ベンチマーク）用コードを反映

--- tests/solve_all_instances.py
+++ tests/solve_all_instances.py
@@ -1,8 +1,10 @@
 from typing import Iterable
 import os
+import sys
 import time
 import subprocess
-
+from concurrent.futures.thread import ThreadPoolExecutor
+import datetime
 
 def enumerate_files(path: str) -> Iterable[str]:
     if os.path.isfile(path):
@@ -12,7 +14,7 @@ def enumerate_files(path: str) -> Iterable[str]:
             yield from enumerate_files(os.path.join(path, child))
 
 
-def execute_trial_sat(instance_file_path: str, log_file_path: str):
+def execute_trial_sat(instance_file_path: str, log_file_path: str) -> tuple[str, str, float]:
 
     log_file = open(log_file_path, "w")
     start_time = time.time()
@@ -33,19 +35,38 @@ def execute_trial_sat(instance_file_path: str, log_file_path: str):
     end_time = time.time()
     log_file.close()
 
-    print(instance_file_path, status, end_time - start_time)
-    
+    return instance_file_path, status, end_time - start_time
+    sys.stdout.flush()
+
+
+def _execute_trial_sat(io_files: tuple[str, str]) -> tuple[str, str, float]:
+    return execute_trial_sat(io_files[0], io_files[1])
+
 
 def main():
 
-    if not os.path.exists("log"):
-        os.mkdir("log")
+    result_dir_path = f"result_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}"
 
-    for instance_file_path in enumerate_files("instance"):
-        if not instance_file_path.endswith(".cnf"):
-            continue
-        log_file_path = os.path.join("log", os.path.basename(instance_file_path).rstrip(".cnf") + ".log")
-        execute_trial_sat(instance_file_path, log_file_path)
+    os.mkdir(result_dir_path)
+    log_dir_path = os.path.join(result_dir_path, "log")
+    os.mkdir(log_dir_path)
+
+    result_file = open(os.path.join(result_dir_path, "result.txt"), "w")
+
+    io_file_paths = [
+        (instance_file_path, os.path.join(log_dir_path, os.path.basename(instance_file_path).rstrip(".cnf") + ".log"))
+        for instance_file_path in enumerate_files("instance") if instance_file_path.endswith(".cnf")
+    ]
+
+    for i, (instance_file_path, status, t) in enumerate(ThreadPoolExecutor(max_workers=1).map(_execute_trial_sat, io_file_paths)):
+        line = "\t".join(map(str, [f"{i + 1}/{len(io_file_paths)}", instance_file_path , status ,t]))
+        print(line)
+        result_file.write(line)
+        result_file.write("\n")
+        result_file.flush()
+        sys.stdout.flush()
+    result_file.close()
+
 
 if __name__ == "__main__":
     main()
